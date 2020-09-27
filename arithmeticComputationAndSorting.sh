0525a2377c07644166f13e76c9594890a069f51e
Komal Sharma <komal.sharma@catseyesystems.com>
2020-09-27 19:25:31 +0530

    getting the inputs from the user

---
+++ arithmeticComputationAndSorting.sh
@@ -0,0 +1,7 @@
+read -p "Enter the value for a :" a
+read -p "Enter the value for b : " b
+read -p "Enter the value for c : " c
+
+echo "Entered value for a :"$a;
+echo "Entered value for b :"$b;
+echo "Entered value for c :"$c;
