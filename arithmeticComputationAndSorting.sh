ddd587297fbd45db0ecc6b9fcfaf97dc4cf24d70
Komal Sharma <komal.sharma@catseyesystems.com>
2020-09-27 19:29:56 +0530

    arithmetic calculation use case 2

--- arithmeticComputationAndSorting.sh
+++ arithmeticComputationAndSorting.sh
@@ -5,3 +5,9 @@ read -p "Enter the value for c : " c
 echo "Entered value for a :"$a;
 echo "Entered value for b :"$b;
 echo "Entered value for c :"$c;
+
+#compute use-case 2
+
+computedValueForUCTwo=$(( a + b * c ));
+echo "USE CASE 2 OUTPUT : "$computedValueForUCTwo;
+
